8706c9d692cc5cec41b210ee28697d13894ca4f3
jacob <jacob_hempel@student.uml.edu>
2017-03-03 11:16:35 -0500

    changed stream to std::vector and adjusted code to compensate

--- include/bitStream.h
+++ include/bitStream.h
@@ -4,6 +4,7 @@
 #define INCLUDE_BITSTREAM_H_
 
 #include <ctime>
+#include <vector>
 
 bool getIntInput(int *inputVar);
 
@@ -14,14 +15,26 @@ class bitStream {
   ~bitStream();
   void scramble();
   void printStream();
+  void setStream(int input[]);
+
   void generateNewStream();
   void generateNewStream(int sizeOfNew);
 
  private:
-  int *stream;
-  int sizeOfStream;
+  std::vector<int> stream;
   unsigned int* seed;
+
   void flipBit(int indexOfBit);
+};
+
+class parity: public bitStream {
+  void appendEvenParityBit();
+  bool checkEvenParity();
+
+
+
+ private:
+  bool parityBit;
 };
 
 #endif  // INCLUDE_BITSTREAM_H_
